68bc0cd4891f3d01ad929d45a038f45436d3aa15
brunojustino <8812217+brunojustino@users.noreply.github.com>
2023-10-15 14:40:43 -0300

    Started project and added a hello request path

---
+++ FirstSteps/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/HelloController.java
@@ -0,0 +1,20 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	private static final String template = "Hello, %s!";
+	private static final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/hello")
+	public Hello hello(@RequestParam(value = "name", defaultValue = "Fodase") String name) {
+		return new Hello(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
